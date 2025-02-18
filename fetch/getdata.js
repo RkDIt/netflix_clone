4b177a8890bb29d35b4f0ea0f1223875772a9c65
RkDit <ritikwins@gmail.com>
2025-02-18 18:55:05 +0530

    slide

--- fetch/getdata.js
+++ fetch/getdata.js
@@ -1,12 +1,31 @@
 fetch('./data/csolData.json')
-.then(response=>response.json())
-.then(data =>  {
-    const movies = data.movies;
-    
-    movies.forEach(movie => {
-        console.log(movie.title)
-    });
-})
-.catch(error=>{
-    console.log(error)
-})
+  .then(response => response.json())
+  .then(movie_data => {
+    const data = movie_data.movies; // data is now the movies array
+
+    let currentIndex = 0;
+    const slideshow = document.getElementById("slideshow");
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const genre = document.getElementById("genre");
+
+    function updateSlideshow() {
+      const movie = data[currentIndex]; // Access the current movie directly from the data array
+      slideshow.style.backgroundImage = `url('${movie.image}')`;
+      title.textContent = movie.title;
+      description.textContent = movie.description;
+      genre.textContent = movie.genre
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % data.length; // Use data.length instead of data.movies.length
+      updateSlideshow();
+    }
+
+    // Initialize Slideshow
+    updateSlideshow();
+    setInterval(nextSlide, 5000); // Change slide every 5 seconds
+  })
+  .catch(error => {
+    console.log(error);
+  });
